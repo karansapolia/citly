0ee871a0b2421afbfdde6e1035fc6e1b8253fa26
Karan Sapolia <sapoliakaran@gmail.com>
2021-08-21 04:16:40 +0530

    Added git hooks, eslint, prettier & rubocop

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  trailingComma: "none",
+  arrowParens: "avoid",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  singleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  proseWrap: "always",
+  endOfLine: "lf"
+};
