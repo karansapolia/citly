422f37c58cab2140554bc7e76af21272cc347321
Karan Sapolia <sapoliakaran@gmail.com>
2021-08-22 16:36:57 +0530

    Add links controller and index action

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class LinksController < ApplicationController
+  def index
+    render
+  end
+end
---
+++ test/controllers/links_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class LinksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
